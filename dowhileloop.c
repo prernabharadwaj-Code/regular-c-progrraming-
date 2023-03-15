5bbbd5099fd6616a1213aa7c55b774fc571e7fe1
prerna bhardwaj <109895343+prernabharadwaj-Code@users.noreply.github.com>
2023-03-15 19:48:52 +0530

    Create dowhileloop.c

---
+++ dowhileloop.c
@@ -0,0 +1,12 @@
+#include"stdio.h"
+int main()
+{
+    int n;
+    // printf("Enter number=");
+    // scanf("%d",&n);
+   do{
+    printf("Enter number=");
+    scanf("%d",&n);
+    }while(n!=0);
+    return 0;
+}
